b6c64784b34f9bc836bdfebf5701e059e55c1ec5
lyjeff <38328932+lyjeff@users.noreply.github.com>
2020-01-03 00:24:14 +0800

    Update hw3

--- hw3/hw3.cpp
+++ hw3/hw3.cpp
@@ -4,25 +4,10 @@
 #include <arpa/inet.h>
 using namespace std;
 
-#define SNAP_LEN 1518     //設定一個封包最多捕獲1518 bytes
-#define SIZE_ETHERNET 14  //header 必定14 bytes
-#define ETHER_ADDR_LEN 6  //address 必定6 bytes
-#define IP_RF 0x8000      //reserved fragment flag
-#define IP_DF 0x4000      //dont fragment flag
-#define IP_MF 0x2000      //more fragments flag
-#define IP_OFFMASK 0x1fff //mask for fragmenting bits
+#define SNAP_LEN 1518    //設定一個封包最多捕獲1518 bytes
+#define SIZE_ETHERNET 14 //header 必定14 bytes
+#define ETHER_ADDR_LEN 6 //address 必定6 bytes
 #define IP_HL(ip) (((ip)->ip_vhl) & 0x0f)
-#define IP_V(ip) (((ip)->ip_vhl) >> 4)
-#define TH_OFF(th) (((th)->th_offx2 & 0xf0) >> 4)
-#define TH_FIN 0x01
-#define TH_SYN 0x02
-#define TH_RST 0x04
-#define TH_PUSH 0x08
-#define TH_ACK 0x10
-#define TH_URG 0x20
-#define TH_ECE 0x40
-#define TH_CWR 0x80
-#define TH_FLAGS (TH_FIN | TH_SYN | TH_RST | TH_ACK | TH_URG | TH_ECE | TH_CWR)
 
 //TCP header
 typedef u_int tcp_seq;
@@ -40,14 +25,6 @@ typedef struct theader
     u_short th_urp; //urgent pointer
 } TCP;
 
-//UDP header
-typedef struct uheader
-{
-    uint16_t uh_sport; //source port
-    uint16_t uh_dport; //destination port
-    uint16_t uh_length;
-    uint16_t uh_sum; //checksum
-} UDP;
 //IP header
 typedef struct iheader
 {
@@ -74,50 +51,62 @@ map<string, int> Packet;
 
 void callback(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 {
-    static int count = 1; //封包計數
-    const IP *ip;         //The IP header
-    const TCP *tcp;       //The TCP header
-    const UDP *udp;       //The UDP header
-    struct tm *lt;        //時間函式
+    static int count = 1;     //封包計數
+    const IP *ip;             //The IP header
+    const TCP *tcp;           //The TCP header
+    const Ethernet *ethernet; // The Ethernet header
+    struct tm *lt;            //時間函式
     char timestr[80];
     time_t local_tv_sec;
-    int size_ip, size_tcp;
+    int size_ip;
+    printf("\n=========================================\nPacket number %d\n\n", count);
+    count++;
 
+    // get time
     local_tv_sec = header->ts.tv_sec;
     lt = localtime(&local_tv_sec);
     strftime(timestr, sizeof(timestr), "%b %d %Y, %X", lt);
+    printf("\tTime: %s\n", timestr);
 
+    ethernet = (Ethernet *)packet;
     ip = (IP *)(packet + SIZE_ETHERNET);
-    size_ip = IP_HL(ip) * 4; //IP header長度
-    if (size_ip < 20)
+    size_ip = IP_HL(ip) * 4; // IP header長度
+    tcp = (TCP *)(packet + SIZE_ETHERNET + size_ip);
+
+    // get MAC source address
+    u_char *mac_string = (unsigned char *)ethernet->ether_shost;
+    printf("\tMac Source Address: %02x:%02x:%02x:%02x:%02x:%02x\n", *(mac_string + 0), *(mac_string + 1), *(mac_string + 2), *(mac_string + 3), *(mac_string + 4), *(mac_string + 5));
+
+    // get MAC destination address
+    mac_string = (unsigned char *)ethernet->ether_dhost;
+    printf("\tMac Destination Address: %02x:%02x:%02x:%02x:%02x:%02x\n", *(mac_string + 0), *(mac_string + 1), *(mac_string + 2), *(mac_string + 3), *(mac_string + 4), *(mac_string + 5));
+
+    if (ethernet->ether_type != 8)
+    {
+        printf("\tProtocol Type: Not IP\n\n");
         return;
-    printf("\n=========================================\nPacket number %d\n\n", count);
-    count++;
+    }
+
     int len = header->len;
     if (ip->ip_p == IPPROTO_TCP) // 計算TCP header offset
-    {
         printf("\tProtocol Type: TCP\n");
-        tcp = (TCP *)(packet + SIZE_ETHERNET + size_ip);
-        printf("\tSource port: %d\n", ntohs(tcp->th_sport));
-        printf("\tDestination port: %d\n", ntohs(tcp->th_dport));
-    }
     else if (ip->ip_p == IPPROTO_UDP) //計算UDP header offset
-    {
         printf("\tProtocol Type: UDP\n");
-        udp = (UDP *)(packet + SIZE_ETHERNET + size_ip);
-        printf("\tSource port: %d\n", ntohs(udp->uh_sport));
-        printf("\tDestination port: %d\n", ntohs(udp->uh_dport));
-        len = ntohs(udp->uh_length);
-    }
     else if (ip->ip_p == IPPROTO_ICMP)
         printf("\tProtocol Type: ICMP\n");
     else if (ip->ip_p == IPPROTO_IP)
         printf("\tProtocol Type: IP\n");
+    else if (ip->ip_p == 89)
+        printf("\tProtocol Type: OSPF\n");
+    else if (ip->ip_p == 91)
+        printf("\tProtocol Type: LARP\n");
+    else if (ip->ip_p == 54)
+        printf("\tProtocol Type: NARP\n");
     else
-        printf("\tProtocol Type: Unknown\n");
-    printf("\tSource IP: %s\n", inet_ntoa(ip->ip_src));
-    printf("\tDestination IP: %s\n", inet_ntoa(ip->ip_dst));
-    printf("\tTime: %s\n", timestr);
+        printf("\tProtocol Type: IP\n");
+    printf("\tSource IP: %s:%d\n", inet_ntoa(ip->ip_src), ntohs(tcp->th_sport));
+    printf("\tDestination IP: %s:%d\n", inet_ntoa(ip->ip_dst), ntohs(tcp->th_dport));
+
     printf("\tPacket Length: %d bytes\n", len);
 
     char tmp[100];
@@ -134,10 +123,8 @@ int main(int argc, char *argv[])
 {
     char *dev = NULL, errbuf[PCAP_ERRBUF_SIZE];
     pcap_t *handle;
-    const char *filter = " "; //過濾
-    struct bpf_program fp;    //compiled filter program (expression)
-    bpf_u_int32 mask, net;
-    int num_packets = -1; //獲取封包數量
+    struct bpf_program fp; //compiled filter program (expression)
+    int num_packets = -1;  //獲取封包數量
 
     if (argc > 2)
     {
@@ -155,9 +142,14 @@ int main(int argc, char *argv[])
     pcap_loop(handle, num_packets, callback, NULL);
     pcap_freecode(&fp);
     pcap_close(handle);
-    printf("\nComplete...\n\n");
+    printf("\n=============\nComplete...\n=============\n\n");
     map<string, int>::iterator ptr;
     ptr = Packet.begin();
+    if (ptr == Packet.end())
+    {
+        printf("No IP Packet...\n");
+        return 0;
+    }
     printf("Packet Counts List:\n");
     printf("===============================================================================\n");
     printf("Source IP\t\t|\tDestination IP\t\t|\tPacket Counts\n");
